9609adf5d992c1bd9c5158b793536c9476d6d47f
CMRJ5421 <christophe.ceyte@orange.com>
2024-05-22 21:50:28 +0200

    Mise en place du formulaire

--- src/app/contact-space/contact-space.component.ts
+++ src/app/contact-space/contact-space.component.ts
@@ -1,12 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-contact-space',
   standalone: true,
-  imports: [],
+  imports: [    
+    FormsModule,
+    CommonModule,
+    RouterModule
+  ],
+    
   templateUrl: './contact-space.component.html',
   styleUrl: './contact-space.component.scss'
 })
 export class ContactSpaceComponent {
-
+  
+  onSubmit(){
+    console.log("Not implemented");
+  }
 }

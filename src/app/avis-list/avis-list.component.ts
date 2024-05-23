4fff05a897ae2a93246339bfc27e1e341b7059d9
CMRJ5421 <christophe.ceyte@orange.com>
2024-05-23 22:15:42 +0200

    ajout d'un mock pour les avis, et afficahge en page d'accueil

--- src/app/avis-list/avis-list.component.ts
+++ src/app/avis-list/avis-list.component.ts
@@ -1,12 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { AVIS } from '../avis/mock-avis';
 
 @Component({
   selector: 'app-avis-list',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
   templateUrl: './avis-list.component.html',
   styleUrl: './avis-list.component.scss'
 })
 export class AvisListComponent {
-
+  avisList=AVIS;
 }
